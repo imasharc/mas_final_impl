9df3db5bed5a3c18c692b048cc816595a90f5062
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-01-13 15:40:11 +0100

    refactor: Update application properties to use file database instead of in-memory
    Data initialization will be always performed
    
    Insert new record into address table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,9 @@
-insert into address (id, appartment_number, city, postal_code, street)
-values (1, null, 'Warsaw', '03-734', 'ul. Targowa 74');
+insert into
+    address (id, appartment_number, city, postal_code, street)
+values
+    (1, null, 'Warsaw', '03-734', 'ul. Targowa 74');
+
+insert into
+    address (id, appartment_number, city, postal_code, street)
+values
+    (2, null, 'Dębica', '39-200', 'ul. Sandomierska 39');
