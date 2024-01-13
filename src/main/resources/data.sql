d044551ea50fe1a4e5d126e98673d1e98638e589
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-01-13 16:11:50 +0100

    style: Make script look cleaner

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,5 @@
 insert into
     address (id, appartment_number, city, postal_code, street)
 values
-    (1, null, 'Warsaw', '03-734', 'ul. Targowa 74');
-
-insert into
-    address (id, appartment_number, city, postal_code, street)
-values
+    (1, null, 'Warsaw', '03-734', 'ul. Targowa 74'),
     (2, null, 'Dębica', '39-200', 'ul. Sandomierska 39');
