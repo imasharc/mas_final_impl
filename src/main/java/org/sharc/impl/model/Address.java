280898517222e15bb833f48767c945bc822c2652
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-01-13 16:10:29 +0100

    refactor(Address): Add ToString method from Lombok

--- src/main/java/org/sharc/impl/model/Address.java
+++ src/main/java/org/sharc/impl/model/Address.java
@@ -7,16 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
 public class Address {
 
     @Id
