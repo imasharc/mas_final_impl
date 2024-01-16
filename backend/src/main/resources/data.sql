b5967999be1d15c271c2b5307feeb418a4956221
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-01-16 21:32:29 +0100

    Update sql script to insert initial account records

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,6 +4,12 @@ VALUES
     (20001, 'Carrier', '000654321', 'Company Two', '2021-02-01', null, '2021-02-02');
 
 insert into
+    account (id, login, password, company_id)
+values
+    (10002, '000123456', 'pancakes', 10001),
+    (20002, '000654321', 'lollipop123', 20001);
+
+insert into
     address (id, appartment_number, city, postal_code, street, company_id)
 values
     (10000, null, 'Warsaw', '03-734', 'ul. Targowa 74', 10001),
