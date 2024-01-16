d5a63616e80d6cdb31bbf077c65296b9c48ed585
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-01-16 23:19:17 +0100

    refactor(Repositories): Add interfaces providing basic CRUD operations for model entities without the need for boilerplate code

---
+++ backend/src/main/java/org/sharc/backend/repository/NationalCarrierRepository.java
@@ -0,0 +1,7 @@
+package org.sharc.backend.repository;
+
+import org.sharc.backend.model.NationalCarrier;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NationalCarrierRepository extends CrudRepository<NationalCarrier, Long> {
+}
---
+++ backend/src/main/java/org/sharc/backend/repository/PrivateCarrierRepository.java
@@ -0,0 +1,7 @@
+package org.sharc.backend.repository;
+
+import org.sharc.backend.model.PrivateCarrier;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PrivateCarrierRepository extends CrudRepository<PrivateCarrier, Long>{
+}
