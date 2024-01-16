0f02f1ea9f8d5403cdc045b7be13b604815009bf
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-01-16 12:25:48 +0100

    refactor(CompanyRepository): Add query method for fetching company related addressses

--- backend/src/main/java/org/sharc/backend/repository/CompanyRepository.java
+++ backend/src/main/java/org/sharc/backend/repository/CompanyRepository.java
@@ -1,7 +1,13 @@
 package org.sharc.backend.repository;
 
 import org.sharc.backend.model.Company;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CompanyRepository extends CrudRepository<Company, Long> {
+    @Query("from Company as c left join fetch c.addresses where c.id = :id")
+    public Optional<Company> findById(@Param("id") Long id);
 }
