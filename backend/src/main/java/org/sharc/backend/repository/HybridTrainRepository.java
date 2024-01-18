de6d75a8c58bfaa387c661ac9bab9494e090fdc9
Antoni Malinowski <antonimalinowski.kontakt@gmail.com>
2024-01-18 13:09:14 +0100

    refactor(HybridTrainRepository): Add interfaces providing basic CRUD operations for model entities without the need for boilerplate code

---
+++ backend/src/main/java/org/sharc/backend/repository/HybridTrainRepository.java
@@ -0,0 +1,7 @@
+package org.sharc.backend.repository;
+
+import org.sharc.backend.model.HybridTrain;
+import org.springframework.data.repository.CrudRepository;
+
+public interface HybridTrainRepository extends CrudRepository<HybridTrain, Long> {
+}
