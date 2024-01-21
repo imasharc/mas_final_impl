158b0fbd47fbabfe19740f939ab0fc5ae120e58a
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-01-21 16:54:30 +0100

    refactor(LeasingAgreementRepository): Add interfaces providing basic CRUD operations for model entities without the need for boilerplate code

---
+++ backend/src/main/java/org/sharc/backend/repository/LeasingAgreementRepository.java
@@ -0,0 +1,7 @@
+package org.sharc.backend.repository;
+
+import org.sharc.backend.model.LeasingAgreement;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LeasingAgreementRepository extends CrudRepository<LeasingAgreement, Long> {
+}
